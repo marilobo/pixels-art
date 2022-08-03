994b5d7e11b5dc62f54949772d7a24fe88be9c7d
Mariana <mlobosilva@outlook.com>
2022-08-03 18:42:09 -0300

    Atende ao requisito 8

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ cor1.classList.add('selected');
 const cor2 = document.getElementById('lavender');
 const cor3 = document.getElementById('violet');
 const cor4 = document.getElementById('lumber');
+const guardaPixel = document.getElementsByClassName('pixel');
 
 for (let index = 1; index < 6; index += 1) {
   let criaSection = document.createElement('section');
   criaSection.className = 'blocosPx';
   quadradoPixels.appendChild(criaSection);
+
   for (let i = 0; i < 5; i += 1) {
     let colunaDiv = document.createElement('div');
     colunaDiv.className = 'pixel';
@@ -27,3 +29,13 @@ cor1.addEventListener('click', trocaSelect);
 cor2.addEventListener('click', trocaSelect);
 cor3.addEventListener('click', trocaSelect);
 cor4.addEventListener('click', trocaSelect);
+
+function trocaCor (parametro) {
+  const selectedElem = document.querySelector('.selected');
+  let idColor = selectedElem.id;
+  parametro.target.id = idColor;
+}
+
+for (let index = 0; index < guardaPixel.length; index += 1) {
+  guardaPixel[index].addEventListener('click', trocaCor);
+}
