a4d87f3257486b85f6028f594d61c036503e9803
Mariana <mlobosilva@outlook.com>
2022-08-03 15:19:28 -0300

    Justifica as linhas e centraliza as informações

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-//Quero imprimir quadrados 5x5 em divs dentro do #pixel-board;
 const quadradoPixels = document.getElementById('pixel-board');
+let corPreta = document.getElementById('black');
+let classSelected = document.get
 
 for (let index = 1; index < 6; index += 1) {
   let criaSection = document.createElement('section');
+  criaSection.className = 'blocosPx';
   quadradoPixels.appendChild(criaSection);
   for (let i = 0; i < 5; i += 1) {
     let colunaDiv = document.createElement('div');
@@ -11,3 +13,5 @@ for (let index = 1; index < 6; index += 1) {
     sectionPai.appendChild(colunaDiv);
   }
 }
+
+
